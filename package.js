aceb63cd11ce95800165988c09c92dce8d90dcf9
Christian Grlica <christian@heavenseven.net>
2015-07-20 19:33:21 +0200

    config useraccounts

--- package.js
+++ package.js
@@ -1,18 +1,33 @@
 
 Package.describe({
-  name: 'heaven7:wsl-useraccounts',
-  version: '0.0.1',
-  summary: 'Auth wrapper package with useraccounts',
-  git: 'https://github.com/heaven7/wsl-useraccounts.git',
-  documentation: 'README.md'
+    name: 'heaven7:wsl-useraccounts',
+    version: '0.0.1',
+    summary: 'Auth wrapper package with useraccounts',
+    git: 'https://github.com/heaven7/wsl-useraccounts.git',
+    documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('wsl-useraccounts.js');
+both = ['client','server'];
 
-  api.use([
-    'useraccounts:unstyled@1.8.1',
-    'accounts-password'
-    ], ['server', 'client'])
+Package.onUse(function(api) {
+    api.versionsFrom('1.1.0.2');
+    api.addFiles('wsl-useraccounts.js');
+
+    api.addFiles([
+        'lib/server/config/accountstemplate.js',
+        'wsl-useraccounts.js'
+    ], both);
+
+    var packages = [
+        'heaven7:wsl-core',
+        'accounts-ui',
+        'accounts-base',
+        'accounts-password',
+        'accounts-twitter',
+        'accounts-facebook',
+        'useraccounts:unstyled@1.9.1'
+    ];
+
+    api.use(packages, ['server', 'client']);
+    api.imply(packages);
 });
