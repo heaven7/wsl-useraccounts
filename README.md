08bce088da61b39e4bb6d3da363540f96b80a323
Christian Grlica <christian@heavenseven.net>
2015-07-20 18:18:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Auth package with useraccounts
---
+++ package.js
@@ -0,0 +1,18 @@
+
+Package.describe({
+  name: 'heaven7:wsl-useraccounts',
+  version: '0.0.1',
+  summary: 'Auth wrapper package with useraccounts',
+  git: 'https://github.com/heaven7/wsl-useraccounts.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('wsl-useraccounts.js');
+
+  api.use([
+    'useraccounts:bootstrap',
+    'accounts-password'
+    ], ['server', 'client'])
+});
